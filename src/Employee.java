770147f7c219de25562af5baa444ab18938fca6a
Elif Savaş <98321352+elifsavas@users.noreply.github.com>
2023-01-29 17:33:11 +0300

    Update Employee.java

--- src/Employee.java
+++ src/Employee.java
@@ -1,2 +1,54 @@
-package PACKAGE_NAME;public class Employee {
+public class Employee {
+    String name;
+    double salary;
+    int workHours;
+    int hireYear;
+
+    Employee(String name, double salary, int workHours, int hireYear) {
+        this.name = name;
+        this.salary = salary;
+        this.workHours = workHours;
+        this.hireYear = hireYear;
+    }
+
+    public String name () {
+        return this.name ;
+    }
+
+    public double tax() {
+        if (this.salary > 1000) {
+            return this.salary * 0.03;
+        }
+        return 0.0 ;
+    }
+
+    public double bonus() {
+        int extraHours = this.workHours - 45 ;
+        if (extraHours>0) {
+            return extraHours * 30;
+        }
+        return 0.0 ;
+    }
+
+    public double raiseSalary() {
+        int year = 2023 - this.hireYear;
+        if (year < 10) {
+            return this.salary * 0.05;
+        } else if (year > 10 && year < 20) {
+            return this.salary * 0.1;
+        } else
+            return this.salary * 0.15;
+
+    }
+
+    public void toString (Employee emp) {
+        System.out.println("Name : " + this.name);
+        System.out.println("Tax : " + this.tax());
+        System.out.println("Bonus : " + this.bonus());
+        System.out.println("Raise of Salary : " + this.raiseSalary());
+
+        double totalSalary = this.salary - this.tax() + this.bonus();
+        System.out.println("Total Salary : " + totalSalary);
+        System.out.println("Salary with raise : " + (this.salary + this.raiseSalary()));
+    }
 }
